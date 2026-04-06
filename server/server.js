ae3f6851facad4bcba285db63ebd7a531c0801f3
Bhumica-jaiswal <bhumicajaiswal@gmail.com>
2026-04-06 19:31:40 +0530

    added server setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config() //to load environment variables from .env file
+import cors from 'cors'
+import http from 'http'
+import { connect } from 'http2'
+import { connectDB } from './lib/db.js'
+
+//create express app
+const app=express()
+const server=http.createServer(app) //cuz socket.io needs http server
+
+//Middleware setup
+app.use(express.json({limit:"4mb"})) //to parse json bodies and set size limit and also send data in json responses
+app.use(cors()) //to connect frontend and backend
+app.use("/api/status", (req, res) => { //a simple route to check if the server is running
+  res.json({ status: "Server is running" });
+});
+//connect to the database
+await(connectDB())
+//define port and start server
+const PORT=process.env.PORT || 5000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
